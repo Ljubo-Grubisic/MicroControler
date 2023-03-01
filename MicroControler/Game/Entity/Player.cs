0aed3ab3826e55755b777d2fe4b5513b4bf5c1f5
ljubo <ljubo@DESKTOP-6CI2MQN>
2023-03-01 10:44:03 +0100

    Solution folder name changed

---
+++ MicroControler/Game/Entity/Player.cs
@@ -0,0 +1,139 @@
+﻿using SFML.System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFML.Graphics;
+using MicroControler.Shapes;
+using MicroControler.Mathematics;
+using MicroControler.Game.RayCasting;
+using MicroControler.Game.Maping;
+
+namespace MicroControler.Game.Entity
+{
+    public class Player : RayCastEable
+    {
+        private Vector2f position;
+        private Vector2f deltaPosition = new Vector2f(0, 0);
+
+        public float Rotation { get; set; } = -(float)Math.PI/2;
+        public float DeltaScale { get; set; } = 25;
+
+        public Rectangle Rectangle { get; set; }
+        public Line FacingLine { get; set; }
+        
+        public Vector2f Position
+        {
+            get { return position; }
+            set
+            {
+                position = value;
+                PositionChanged();
+                DeltaPositionChanged();
+            }
+        }
+        public float PositionX
+        {
+            get { return position.X; }
+            set
+            {
+                position.X = value;
+                PositionChanged();
+                DeltaPositionChanged();
+            }
+        }
+        public float PositionY
+        {
+            get { return position.Y; }
+            set
+            {
+                position.Y = value;
+                PositionChanged();
+                DeltaPositionChanged();
+            }
+        }
+
+        public Vector2f DeltaPosition
+        {
+            get { return deltaPosition; }
+            set
+            {
+                deltaPosition = value;
+                DeltaPositionChanged();
+            }
+        }
+        public float DeltaPositionX
+        {
+            get { return deltaPosition.X; }
+            set
+            {
+                deltaPosition.X = value;
+                DeltaPositionChanged();
+            }
+        }
+        public float DeltaPositionY
+        {
+            get { return deltaPosition.Y; }
+            set
+            {
+                deltaPosition.Y = value;
+                DeltaPositionChanged();
+            }
+        }
+
+        static private Vector2f loadSize { get; } = new Vector2f(8f, 8f);
+
+        public Player(Vector2f position)
+        {
+            this.position = position;
+            this.deltaPosition.X = (float)(Math.Cos(this.Rotation));
+            this.deltaPosition.Y = (float)(Math.Sin(this.Rotation));
+            this.FacingLine = new Line(this.Position, MathHelper.AddDeltaPosition(this.Position, this.DeltaPosition, DeltaScale))
+            {
+                OutlineColor = Color.Yellow
+            };
+            this.Rectangle = new Rectangle(MathHelper.CenterRectangle(position, loadSize), loadSize.X, loadSize.Y)
+            {
+                FillColor = Color.Yellow
+            };
+        }
+        public Player(float positionX, float positionY)
+        {
+            this.position = new Vector2f(positionX, positionY);
+            this.deltaPosition.X = (float)(Math.Cos(this.Rotation));
+            this.deltaPosition.Y = (float)(Math.Sin(this.Rotation));
+            this.FacingLine = new Line(this.Position, MathHelper.AddDeltaPosition(this.Position, this.DeltaPosition, DeltaScale))
+            {
+                OutlineColor = Color.Yellow
+            };
+            this.Rectangle = new Rectangle(MathHelper.CenterRectangle(positionX, positionY, loadSize), loadSize.X, loadSize.Y)
+            {
+                FillColor = Color.Yellow
+            };
+        }
+
+        public void Draw(RenderWindow window, Map map)
+        {
+            Rectangle.Position += (Vector2f)map.MapWindow.Position;
+            FacingLine.Position0 += (Vector2f)map.MapWindow.Position;
+            FacingLine.Position1 += (Vector2f)map.MapWindow.Position;
+            window.Draw(this.Rectangle);
+            window.Draw(this.FacingLine);
+            FacingLine.Position0 -= (Vector2f)map.MapWindow.Position;
+            FacingLine.Position1 -= (Vector2f)map.MapWindow.Position;
+            Rectangle.Position -= (Vector2f)map.MapWindow.Position;
+        }
+
+        public void PositionChanged()
+        {
+            this.Rectangle.Position = MathHelper.CenterRectangle(this.Position, loadSize);
+        }
+
+        public void DeltaPositionChanged()
+        {
+            this.FacingLine.Position0 = this.Position;
+            this.FacingLine.Position1 = MathHelper.AddDeltaPosition(this.Position, this.DeltaPosition, DeltaScale);
+        }
+    }
+}
---
+++ MicroControler/Shapes/Rectangle.cs
@@ -0,0 +1,100 @@
+﻿using SFML.Graphics;
+using SFML.System;
+
+namespace MicroControler.Shapes
+{
+    public class Rectangle : ConvexShape
+    {
+        private Vector2f position;
+        private float width;
+        private float height;
+
+        public new Vector2f Position
+        {
+            get { return position; }
+            set
+            {
+                position = value;
+                DataChanged();
+            }
+        }
+        public float PositionX
+        {
+            get { return position.X; }
+            set
+            {
+                position.X = value;
+                DataChanged();
+            }
+        }
+        public float PositionY
+        {
+            get { return position.Y; }
+            set
+            {
+                position.Y = value;
+                DataChanged();
+            }
+        }
+        public float Width
+        {
+            get { return width; }
+            set
+            {
+                width = value;
+                DataChanged();
+            }
+        }
+        public float Height
+        {
+            get { return height; }
+            set
+            {
+                height = value;
+                DataChanged();
+            }
+        }
+
+        public Rectangle(Vector2f position, float width, float height) : base(4)
+        {
+            Position = position;
+            Width = width;
+            Height = height;
+            DataChanged();
+        }
+        public Rectangle(Vector2f position, Vector2f size) : base(4)
+        {
+            Position = position;
+            Width = size.X;
+            Height = size.Y;
+            DataChanged();
+        }
+        public Rectangle(float positionX, float positionY, Vector2f size) : base(4)
+        {
+            Position = new Vector2f(positionX, positionY);
+            Width = size.X;
+            Height = size.Y;
+            DataChanged();
+        }
+        public Rectangle(float positionX, float positionY, float width, float height) : base(4)
+        {
+            Position = new Vector2f(positionX, positionY);
+            Width = width;
+            Height = height;
+            DataChanged();
+        }
+
+        public void Draw(RenderWindow window)
+        {
+            window.Draw(this);
+        }
+
+        private void DataChanged()
+        {
+            this.SetPoint(0, new Vector2f(PositionX, PositionY));
+            this.SetPoint(1, new Vector2f(PositionX + Width, PositionY));
+            this.SetPoint(3, new Vector2f(PositionX, PositionY + Height));
+            this.SetPoint(2, new Vector2f(PositionX + Width, PositionY + Height));
+        }
+    }
+}
